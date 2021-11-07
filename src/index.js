d4dd2575b86b46beaf980f5291668973455135fe
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-11-07 23:37:33 +0300

    Добавляет файлы html apiService

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import './sass/main.scss';
+import './styles.css';
+import PixabayService from "./js/apiService";
+
+const searchForm = document.querySelector(".js-form");
+const picturesContainer = document.querySelector(".js-pictures-container");
+
+const apiService = new PixabayService();
+
+searchForm.addEventListener("submit", onSearch);
+
+function onSearch(event) {
+    event.preventDefault();
+
+    apiService.query = event.currentTarget.elements.query.value;
+    apiService.resetPage();
+    apiService.fetchPictures();
+
+
+}
---
+++ src/js/apiService.js
@@ -0,0 +1,29 @@
+export default class PixabayService {
+    constructor() {
+        this.searchQuery = "";
+        this.page = 1;
+
+    }
+    fetchPictures() {
+        const KEY = "24078076-056bd2e530cc19b75a9dfc811";
+        const url = `https://pixabay.com/api/?key=${KEY}&q=${this.searchQuery}&per_page=10&page=${this.page}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.page += 1;
+            });
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
